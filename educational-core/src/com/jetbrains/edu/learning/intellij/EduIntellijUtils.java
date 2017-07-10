ac349fdc0af4465f8d5cb6f7f37253430cf121cc
liana.bakradze <liana.bakradze@jetbrains.com>
2017-07-10 13:09:12 +0300

    minor cleanup

--- educational-core/src/com/jetbrains/edu/learning/intellij/EduIntellijUtils.java
+++ educational-core/src/com/jetbrains/edu/learning/intellij/EduIntellijUtils.java
@@ -4,17 +4,14 @@ import com.intellij.codeInsight.daemon.impl.quickfix.OrderEntryFix;
 import com.intellij.execution.junit.JUnitExternalLibraryDescriptor;
 import com.intellij.ide.fileTemplates.FileTemplate;
 import com.intellij.ide.fileTemplates.FileTemplateManager;
-import com.intellij.ide.projectView.actions.MarkRootActionBase;
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard;
 import com.intellij.ide.util.newProjectWizard.StepSequence;
 import com.intellij.ide.util.projectWizard.ProjectBuilder;
 import com.intellij.lang.Language;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.roots.ui.configuration.actions.NewModuleAction;
@@ -43,41 +40,6 @@ public class EduIntellijUtils {
 
     private EduIntellijUtils() {
     }
-
-  private static void commitAndSaveModel(final ModifiableRootModel model) {
-        ApplicationManager.getApplication().runWriteAction(new Runnable() {
-            @Override
-            public void run() {
-                model.commit();
-                model.getProject().save();
-            }
-        });
-    }
-
-    @Nullable
-    private static ModifiableRootModel getModel(@NotNull VirtualFile dir, @NotNull Project project) {
-        final Module module = ModuleUtilCore.findModuleForFile(dir, project);
-        if (module == null) {
-            LOG.info("Module for " + dir.getPath() + " was not found");
-            return null;
-        }
-        return ModuleRootManager.getInstance(module).getModifiableModel();
-    }
-
-    static void markDirAsSourceRoot(@NotNull final VirtualFile dir, @NotNull final Project project) {
-        final ModifiableRootModel model = getModel(dir, project);
-        if (model == null) {
-            return;
-        }
-        final ContentEntry entry = MarkRootActionBase.findContentEntry(model, dir);
-        if (entry == null) {
-            LOG.info("Content entry for " + dir.getPath() + " was not found");
-            return;
-        }
-        entry.addSourceFolder(dir, false);
-        commitAndSaveModel(model);
-    }
-
 
     public static void addTemplate(@NotNull final Project project, @NotNull VirtualFile baseDir, @NotNull @NonNls final String templateName) {
         final FileTemplate template = FileTemplateManager.getInstance(project).getInternalTemplate(templateName);
