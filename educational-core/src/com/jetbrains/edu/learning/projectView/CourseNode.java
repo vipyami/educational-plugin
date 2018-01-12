6cf3877969da4dec650d3cf95277980bd5c862a7
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2018-01-12 15:10:30 +0300

    EDU-1278: find task psi directory by its name

--- educational-core/src/com/jetbrains/edu/learning/projectView/CourseNode.java
+++ educational-core/src/com/jetbrains/edu/learning/projectView/CourseNode.java
@@ -56,12 +56,14 @@ public class CourseNode extends EduNode {
     if (directory.getChildren().length > 0 && lessons.size() == 1) {
       final List<Task> tasks = lesson.getTaskList();
       if (tasks.size() == 1) {
-        PsiDirectory taskDirectory = (PsiDirectory)directory.getChildren()[0];
-        PsiDirectory srcDir = taskDirectory.findSubdirectory(EduNames.SRC);
-        if (srcDir != null) {
-          taskDirectory = srcDir;
+        PsiDirectory taskDirectory = directory.findSubdirectory(EduNames.TASK + 1);
+        if (taskDirectory != null) {
+          PsiDirectory srcDir = taskDirectory.findSubdirectory(EduNames.SRC);
+          if (srcDir != null) {
+            taskDirectory = srcDir;
+          }
+          return new TaskNode(myProject, taskDirectory, myViewSettings, tasks.get(0));
         }
-        return new TaskNode(myProject, taskDirectory, myViewSettings, tasks.get(0));
       }
     }
     return new LessonNode(myProject, directory, myViewSettings, lesson);
