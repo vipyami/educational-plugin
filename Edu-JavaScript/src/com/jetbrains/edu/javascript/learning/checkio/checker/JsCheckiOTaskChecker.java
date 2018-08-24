532e568f0759b8fd0184492a3934a2f1dbb60977
Nikita Voloshin <nik.volosh@yandex.ru>
2018-08-24 12:15:21 +0300

    EDU-1607: Common checking logic is extracted to `educational-core` from `Edu-Python`. Implemented checking for js.CheckiO missions.

--- Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/checker/JsCheckiOTaskChecker.java
+++ Edu-JavaScript/src/com/jetbrains/edu/javascript/learning/checkio/checker/JsCheckiOTaskChecker.java
@@ -1,33 +1,34 @@
-package com.jetbrains.edu.python.learning.checkio.checker;
+package com.jetbrains.edu.javascript.learning.checkio.checker;
 
+import com.intellij.lang.javascript.JavaScriptFileType;
 import com.intellij.openapi.project.Project;
+import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOApiConnector;
+import com.jetbrains.edu.javascript.learning.checkio.connectors.JsCheckiOOAuthConnector;
 import com.jetbrains.edu.learning.checkio.CheckiOCourseContentGenerator;
 import com.jetbrains.edu.learning.checkio.CheckiOCourseUpdater;
 import com.jetbrains.edu.learning.checkio.checker.CheckiOMissionCheck;
 import com.jetbrains.edu.learning.checkio.checker.CheckiOTaskChecker;
 import com.jetbrains.edu.learning.checkio.courseFormat.CheckiOCourse;
 import com.jetbrains.edu.learning.courseFormat.tasks.EduTask;
-import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOApiConnector;
-import com.jetbrains.edu.python.learning.checkio.connectors.PyCheckiOOAuthConnector;
-import com.jetbrains.python.PythonFileType;
 import org.jetbrains.annotations.NotNull;
 
-public class PyCheckiOTaskChecker extends CheckiOTaskChecker {
-  protected PyCheckiOTaskChecker(@NotNull EduTask task, @NotNull Project project) {
-    super(task, project, PyCheckiOOAuthConnector.getInstance());
+public class JsCheckiOTaskChecker extends CheckiOTaskChecker {
+
+  protected JsCheckiOTaskChecker(@NotNull EduTask task, @NotNull Project project) {
+    super(task, project, JsCheckiOOAuthConnector.getInstance());
   }
 
   @NotNull
   @Override
   protected CheckiOMissionCheck getMissionCheck() {
-    return new PyCheckiOMissionCheck(project, task);
+    return new JsCheckiOMissionCheck(project, task);
   }
 
   @NotNull
   @Override
   protected CheckiOCourseUpdater getCourseUpdater() {
     final CheckiOCourseContentGenerator contentGenerator =
-      new CheckiOCourseContentGenerator(PythonFileType.INSTANCE, PyCheckiOApiConnector.getInstance());
+      new CheckiOCourseContentGenerator(JavaScriptFileType.INSTANCE, JsCheckiOApiConnector.getInstance());
 
     return new CheckiOCourseUpdater(
       (CheckiOCourse) task.getCourse(),
